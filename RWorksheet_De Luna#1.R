cec7dc199089b77f8e9fac2c0a0382277c0876b6
Nicolecoline <nicole.deluna@students.isatu.edu.ph>
2023-09-20 04:07:55 +0000

    worksheet1

---
+++ RWorksheet_De Luna#1.R
@@ -0,0 +1,69 @@
+#vectorAge
+
+ age <-c(34, 28, 22, 36, 27, 18, 52, 39, 42, 29,
+          35, 31, 27, 22, 37, 34, 19, 20, 57, 49, 50, 37, 46, 25, 17, 37, 42, 53, 41,
+          51, 35, 24, 33, 41.)
+  
+
+#data_points
+  length(age)
+  
+#reciprocal_age
+  reciprocal_age <-1/age
+  reciprocal_age
+
+#new_age
+  new_age <- c (age,0,age)
+  new_age
+  
+#sort_the_value  
+  sorted_age <- sort(age)
+  sorted_age
+  
+#minimum and maximum value of age
+  min_age <-min(age)
+  max_age <-max(age)
+  
+  min_age
+  max_age
+
+#data
+  data <-c(2.4, 2.8, 2.1, 2.5, 2.4, 2.2, 2.5, 2.3,
+           2.5, 2.3, 2.4,2.7)
+  data
+  
+#new vector for data where double every value of the data
+  doubled_data <- c(data*2)
+  doubled_data
+  
+#sequence
+  int_1to100 <- seq (1,100)
+  int_1to100
+  
+ int_20to60 <- seq (20,60)
+ int_20to60
+  
+ int_51to91<- seq (51,91)
+ int_51to91
+ 
+ int_1to1000 <- seq(1,1000)
+ int_1to1000
+ 
+ 
+ 
+ 
+ 
+ 
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
